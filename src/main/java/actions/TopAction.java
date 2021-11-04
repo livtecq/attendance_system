e04715a86e6339157c80efebd7a9b1940b9df8e3
nishiki.nagayasu <nishiki1202@gmail.com>
2021-11-04 22:33:58 +0900

    「TopActionの修正」

--- src/main/java/actions/TopAction.java
+++ src/main/java/actions/TopAction.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.AttendanceView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import services.AttendanceService;
 
 /**
  * トップページに関する処理を行うActionクラス
@@ -13,14 +15,19 @@ import constants.ForwardConst;
  */
 public class TopAction extends ActionBase {
 
+    private AttendanceService service;
+
     /**
      * indexメソッドを実行する
      */
     @Override
     public void process() throws ServletException, IOException {
 
+        service = new AttendanceService();
+
         //メソッドを実行
         invoke();
+        service.close();
 
     }
 
@@ -28,6 +35,10 @@ public class TopAction extends ActionBase {
      * 一覧画面を表示する
      */
     public void index() throws ServletException, IOException {
+
+        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+        putRequestScope(AttributeConst.ATTENDANCE, new AttendanceView()); //空の従業員インスタンス
+
 
         //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
         String flush = getSessionScope(AttributeConst.FLUSH);
@@ -38,6 +49,10 @@ public class TopAction extends ActionBase {
 
         //一覧画面を表示
         forward(ForwardConst.FW_TOP_INDEX);
+    }
+
+    public void create() throws ServletException, IOException {
+
     }
 
 }
